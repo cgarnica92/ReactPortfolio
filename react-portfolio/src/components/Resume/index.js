e128958f0afb68d57b9c0abf3d064cb646af2265
cgarnica92 <c_garnica92@yahoo.com>
2023-01-17 18:48:36 -0500

    Added Resume Layout

--- react-portfolio/src/components/Resume/index.js
+++ react-portfolio/src/components/Resume/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 
 function Resume  () {
-    <div>
-        
-    </div>
+
+    return(
+        <div className="aboutMain">
+            <section className="section">
+                <div className="box">
+                    <h1>Stuff</h1>
+                </div>
+            </section>
+            <section className="section">
+                <div className="box">
+                    <h1>Other Stuff</h1>
+                </div>
+            </section>
+            <section className="section">
+                <div className="box">
+                    <h1>Profiles and Resume</h1>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Resume; 
