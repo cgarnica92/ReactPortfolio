342b16acc892f8e1996873af7f1e8ac556e556e8
cgarnica92 <c_garnica92@yahoo.com>
2023-01-16 19:34:35 -0500

    added contact layout and form

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -3,7 +3,34 @@ import React from "react";
 function Contact () {
     return(
         <div className="columns aboutMain">
-            
+            <div className="column is-one-fifth box">
+                <p>Contact Details</p>
+            </div>
+            <div className="column section box">
+                <div className="field">
+                    <label className="label">Name</label>
+                    <div className="control">
+                        <input className="input" type="text" placeholder="Your Name"></input>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control">
+                        <input className="input" type="email" placeholder="Your Email"></input>
+                    </div>
+                </div>
+                <div className="field">
+                    <label className="label">Message</label>
+                    <div className="control">
+                        <textarea className="textarea" placeholder="Add Your Message Here."></textarea>
+                    </div>
+                </div>
+                <div className="field">
+                    <div className="control">
+                        <button className="button">Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
